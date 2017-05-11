3f3ba59f11d5132ebc70da9b88c5718467f88968
briavers <brian.verschoore@student.arteveldehs.be>
2017-05-11 14:51:31 +0200

    11052017

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// get stuff to full height
+  var fullHeight = window.innerHeight;
+  console.log(fullHeight);
+
+
+
+window.onload = function(){
+function changeElementHeight() {
+  
+    var el = document.getElementsByClassName('fullHeight');
+    
+    el.style.height = fullHeight;
+    
+};
+}
+
